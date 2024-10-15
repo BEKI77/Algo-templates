c12c5b028ad7f041b9aa3bbf0cf7ec4370e6c635
BEKI77 <berekettadiwos00@gmail.com>
2024-10-15 11:07:00 +0300

    merge sort

---
+++ Floyd-warshall.py
@@ -0,0 +1,12 @@
+def floyd_warshall(n, edges):
+    dist = [[float('inf')] * n for _ in range(n)]
+    for i, j, w in edges:
+        dist[i][j] = dist[j][i] = w
+    for i in range(n):
+        dist[i][i] = 0
+    for k in range(n):
+        for i in range(n):
+            for j in range(n):
+                dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j])
+    
+    return dist
